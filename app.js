0bb07b0f4dd8efe114eb84a3bfce53e3dd09bb68
Jacob Gasser <jacobgasser17@gmail.com>
2020-11-23 17:17:23 -0600

    Style Update

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 
 app.set("view engine", "pug")
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'files')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render('index.pug')
