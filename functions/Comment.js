993984e9df58b9f1d91a143fbf953f51cb83d39f
Tranquill6 <rileyredfern@gmail.com>
2020-12-04 11:35:55 -0600

    formatted comment.js and ratemovie.js

--- functions/Comment.js
+++ functions/Comment.js
@@ -4,3 +4,18 @@ add comment
 remove comment
 edit comment
 */
+const mysql = require('mysql');
+const database = require('./connect.js')
+const db = database.db
+
+const makeComment = (text) => {
+
+}
+
+const removeComment = (id) => {
+
+}
+
+const editComment = (id, text) => {
+    
+}
