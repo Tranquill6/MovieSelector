d6e0e5d03d397df23ceef60971fff35414807e9f
Tranquill6 <rileyredfern@gmail.com>
2020-12-04 12:03:45 -0600

    queries for comment.js

--- functions/Comment.js
+++ functions/Comment.js
@@ -8,14 +8,27 @@ const mysql = require('mysql');
 const database = require('./connect.js')
 const db = database.db
 
-const makeComment = (text) => {
+const makeComment = (text, movieID) => {
+    sql = `INSERT INTO Comments('MovieId', 'content') VALUES (*${db.escape(movieID)}*,*${db.escape(text)}*)`;
+    db.query(sql, (error, result) => {
+        if (err) throw err;
+        console.log(result);
 
+    })
 }
 
 const removeComment = (id) => {
-
+    sql = `DELETE FROM Comments WHERE commentId *${db.escape(id)}*`;
+    db.query(sql, (error, result) => {
+        if (err) throw err;
+        console.log(result);
+    })
 }
 
 const editComment = (id, text) => {
-    
+    sql = `UPDATE Comments SET content *${db.escape(text)}* WHERE commentId *${db.escape(id)}*`;
+    db.query(sql, (error, result) => {
+        if (err) throw err;
+        console.log(result);
+    })
 }
