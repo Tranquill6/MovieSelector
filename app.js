18de6d24bf936d2938296418203dbd5f056b5a66
Jacob Gasser <jacobgasser17@gmail.com>
2020-12-04 13:06:31 -0600

    formatting

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.render('index.pug')
+    res.render('search.pug')
 })
 
 app.get('/Search', (req, res) => {
