077026366197702a15be7418d10c45c235bd3ff8
Jacob Gasser <jacobgasser17@gmail.com>
2020-12-04 14:30:06 -0600

    added sql to similar movie

--- functions/SimilarMovie.js
+++ functions/SimilarMovie.js
@@ -2,7 +2,9 @@ const database = require('./connect.js')
 const db = database.db
 
 const GetMovie = (id) => {
-
+    sql = `SELECT t1.viewID
+    FROM Tags t1
+    WHERE t1.tagID IN (SELECT t1.tagID FROM Tags t1, TagTitles t2 WHERE ${id} = t1.viewID AND t2.TagId = t1.tagID AND MOD(t1.tagID,2) != 1) AND t1.viewID != ${id} LIMIT 1`
 }
 
 module.exports = {
