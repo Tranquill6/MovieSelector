ba9ae8aa09c34a830e2a70e727d7521caf525d97
Tranquill6 <rileyredfern@gmail.com>
2020-12-07 12:27:30 -0600

    rating stuff pt2

--- app.js
+++ app.js
@@ -72,6 +72,13 @@ app.get('/movie/:movieId', (req, res) => {
     })
 })
 
+app.post('/ratemovie/:movieId'), (req, res) => {
+    movieId = req.params['movieId']
+    Rate.addRating(movieId, req.body.rating).then((results) => {
+        res.send(results)
+    })
+}
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
