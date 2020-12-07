e5f17da5dd66e81d69a0dcb3d4de2307af8c478a
Tranquill6 <rileyredfern@gmail.com>
2020-12-07 11:48:06 -0600

    rating stuff

--- functions/RateMovie.js
+++ functions/RateMovie.js
@@ -7,14 +7,22 @@ const mysql = require('mysql');
 const database = require('./connect.js')
 const db = database.db
 
-const addRating = (rating) => {
+const addRating = (id, userRating) => {
     return new Promise((resolve,reject) => {
-        
-    })
-}
+        sql = `SELECT rtAllCriticsRating, rtAllCriticsNumReviews FROM Movies WHERE movieId='${db.escape(id)}'`
+        db.query(sql, (err, result) => {
+            if (err) throw err
+            rating = result.rtAllCriticsRating
+            numOfReviews = result.rtAllCriticsNumReviews
 
-const calculateRating = () => {
-    return new Promise((resolve,reject) => {
+            newRating = (rating*numOfReviews+userRating)/(numOfReviews+1)
+            finalRating = newRating.toFixed(1)
 
+            sql = `UPDATE Movies SET rtAllCriticsRating='${db.escape(finalRating)}', rtAllCriticsNumReviews='${db.escape(numOfReviews+1)}' WHERE movieId='${db.escape(id)}'`
+            db.query(sql, (err, result) => {
+                if (err) throw err
+                console.log(result)
+            })
+        })
     })
 }
