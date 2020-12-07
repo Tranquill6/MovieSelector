2c27ac71c7eb04ac14c2c8ddc5176af9caf3d549
Tranquill6 <rileyredfern@gmail.com>
2020-12-07 14:58:34 -0600

    clean up

--- functions/RateMovie.js
+++ functions/RateMovie.js
@@ -15,13 +15,10 @@ const addRating = (id, userRating) => {
             if (err) throw err
             rating = parseFloat(result[0].rtAllCriticsRating)
             numOfReviews = parseInt(result[0].rtAllCriticsNumReviews)
-            console.log(rating)
-            console.log(numOfReviews)
+            
             newRating = (rating*numOfReviews+parseFloat(userRating))/(numOfReviews+1)
             finalRating = newRating.toFixed(1)
             numOfReviews += 1
-            console.log(finalRating)
-            console.log(numOfReviews)
 
             sql = `UPDATE Movies SET rtAllCriticsRating='${finalRating}', rtAllCriticsNumReviews='${numOfReviews}' WHERE movieId='${id}'`
             db.query(sql, (err, result) => {
