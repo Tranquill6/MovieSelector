a4d3e8f39aa22f9282283b957ad75db9230f0b6c
Jacob Gasser <jacobgasser17@gmail.com>
2020-12-07 12:11:53 -0600

    movie.pug

--- public/movie.js
+++ public/movie.js
@@ -1,3 +1,8 @@
 window.addEventListener('load', function () {
-    document.getElementById('rateBtn')
+    document.getElementById('rateBtn').addEventListener('change', () =>{
+        rating = document.getElementById('rateBtn')
+        if(!(rating.value > 0 && rating.value < 10) && !isNaN(rating.value)){
+
+        }
+    })
 });
