3ac8199e50829d472dda26fe21ddccc8904e7ed1
Jacob Gasser <jacobgasser17@gmail.com>
2020-12-07 13:41:23 -0600

    improved the cyclic nature of the similar movie page

--- functions/SimilarMovie.js
+++ functions/SimilarMovie.js
@@ -5,11 +5,15 @@ const GetMovie = (id) => {
     return new Promise( async (resolve,reject) => {
         sql = `SELECT t1.movieId
             FROM Tags t1
-            WHERE t1.tagId IN (SELECT t1.tagId FROM Tags t1, TagTitles t2 WHERE ${id} = t1.movieId AND t2.tagId = t1.tagId AND MOD(t1.tagId,2) != 1) AND t1.movieId != ${id} LIMIT 1`
+            WHERE t1.tagId IN (SELECT t1.tagId FROM Tags t1, TagTitles t2 WHERE ${id} = t1.movieId AND t2.tagId = t1.tagId AND MOD(t1.tagId,2) != 1) AND t1.movieId != ${id} LIMIT 10`
         const ID = await new Promise((rslv, rjct) => db.query(sql, (err, result) => {
             if (err) throw err
-            rslv(result[0].movieId)
-            rjct(result[0].movieId)
+            ident = 0
+            result.forEach(element => {
+                ident = element.movieId
+            });
+            rslv(ident)
+            rjct(ident)
         }))
         resolve(ID)
     })
